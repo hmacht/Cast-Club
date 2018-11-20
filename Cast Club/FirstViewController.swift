e7def332e1c0b7d3e29e0308d984e84358dbe702
Henry Macht <henrymacht@henrys-macbook-pro.local>
2018-11-20 16:59:09 -0500

    Initial Commit

---
+++ Cast Club/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Cast Club
+//
+//  Created by Henry Macht on 11/20/18.
+//  Copyright © 2018 Henry Macht. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Cast Club/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cast Club
+//
+//  Created by Henry Macht on 11/20/18.
+//  Copyright © 2018 Henry Macht. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

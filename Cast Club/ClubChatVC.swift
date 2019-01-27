26924d71a99a37e80751ca0078f49c5a047c9a9a
Henry Macht <henrymacht@henrys-macbook-pro.local>
2019-01-27 12:32:45 -0500

    added more to club chat page

--- Cast Club/ClubChatVC.swift
+++ Cast Club/ClubChatVC.swift
@@ -28,11 +28,24 @@ class ClubChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.separatorStyle = .none
         
         self.tableView.backgroundColor = UIColor(red: 246.0/255.0, green: 246.0/255.0, blue: 250.0/255.0, alpha: 1.0)
+        
+        let rightBarButtonItem = UIBarButtonItem.init(image: UIImage(named: "Group 654"), style: .done, target: self, action: #selector(ClubChatVC.filter))
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        
+        
+        
     }
     
+//    override func viewDidAppear(_ animated: Bool) {
+//        self.navigationController?.navigationBar.topItem?.title = "Longer Club Name"
+//        //navigationController?.navigationBar.topItem?.titleView?.alpha = 0.4
+//        //self.navigationController?.navigationBar.barTintColor = UIColor(red: 64.0/255.0, green: 64.0/255.0, blue: 64.0/255.0, alpha: 1.0)
+//        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 64.0/255.0, green: 64.0/255.0, blue: 64.0/255.0, alpha: 0.0)]
+//    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-        
+        return 6
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,7 +55,7 @@ class ClubChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             headerCell.profileImage.image = UIImage(named: "img1")
             headerCell.profileImage.layer.cornerRadius = 30.0
             headerCell.profileImage.clipsToBounds = true
-            headerCell.nameLabel.text = "Clubs Name!"
+            headerCell.nameLabel.text = "Longer Club Name"
             headerCell.totalMembersLabel.text = "450 members"
             
             headerCell.followButton.setTitle("Follow", for: .normal)
@@ -62,14 +75,15 @@ class ClubChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             myCell.usersProfileImage.image = UIImage(named: "img1")
             myCell.usersProfileImage.layer.cornerRadius = 20.0
             myCell.usersProfileImage.clipsToBounds = true
+            myCell.usernameLabel.sizeToFit()
             myCell.usernameLabel.text = "Username"
-            myCell.responcelabel.text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt"
+            myCell.responcelabel.text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt Lorem ipsum dolor sit amet."
             myCell.responcelabel.sizeToFit()
             myCell.responcelabel.numberOfLines = 50
             
             myCell.contentView.backgroundColor = UIColor(red: 246.0/255.0, green: 246.0/255.0, blue: 250.0/255.0, alpha: 1.0)
             
-            let whiteRoundedView : UIView = UIView(frame: CGRect(x: 0, y: 8, width: self.view.frame.size.width, height: 149))
+            let whiteRoundedView : UIView = UIView(frame: CGRect(x: 0, y: 8, width: self.view.frame.size.width, height: myCell.frame.height - 10))
             
             
             whiteRoundedView.layer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [255.0, 255.0, 255.0, 1.0])
@@ -90,6 +104,38 @@ class ClubChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        if tableView.contentOffset.y > 60 {
+            if let navController = navigationController {
+                navController.navigationBar.topItem?.title = "Longer Club Name"
+            }
+            let rightBarButtonItem = UIBarButtonItem.init(image: UIImage(named: "Group 653"), style: .done, target: self, action: #selector(ClubChatVC.post))
+            self.navigationItem.rightBarButtonItem = rightBarButtonItem
+            print("larger")
+        } else {
+            if let navController = navigationController {
+                navController.navigationBar.topItem?.title = ""
+            }
+            let rightBarButtonItem = UIBarButtonItem.init(image: UIImage(named: "Group 654"), style: .done, target: self, action: #selector(ClubChatVC.filter))
+            self.navigationItem.rightBarButtonItem = rightBarButtonItem
+            print("smaller")
+        }
+//        if tableView.contentOffset.y < 100 && tableView.contentOffset.y > 0 {
+//            if let navController = navigationController {
+//                print(tableView.contentOffset.y / 100)
+//                //navController.navigationBar.topItem?.titleView?.alpha = tableView.contentOffset.y / 100
+//                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 64.0/255.0, green: 64.0/255.0, blue: 64.0/255.0, alpha: tableView.contentOffset.y / 100)]
+//            }
+//
+//            if tableView.contentOffset.y < 0{
+//                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 64.0/255.0, green: 64.0/255.0, blue: 64.0/255.0, alpha: 0.0)]
+//            }
+//        }
+    }
+    
+    
+    
     
     
     @objc func follow(){
@@ -106,6 +152,10 @@ class ClubChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @objc func post(){
         self.performSegue(withIdentifier: "toPost", sender: self)
+    }
+    
+    @objc func filter(){
+        print("FILTER")
     }
     
 
